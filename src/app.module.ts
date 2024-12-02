1fe01538c5460966098196d0e45051f6ef07a50c
Enzo Glauber <enzojob@gmail.com>
2024-12-01 21:42:33 -0300

    base to addres and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
+import { UserModule } from './user/user.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule, AddressModule],
   controllers: [AppController],
   providers: [AppService]
 })
